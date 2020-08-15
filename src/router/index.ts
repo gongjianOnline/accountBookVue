368a6d2224c0d554977391c02185290b523f020f
龚箭 <15031617367@163.com>
2020-08-15 14:28:17 +0800

    添加404

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import Money from "@/views/Money.vue";
 import Statistacs from "@/views/Statistacs.vue";
 import Labels from "@/views/Labels.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter)
 
@@ -27,6 +28,10 @@ Vue.use(VueRouter)
       name: 'statistacs',
       component: Statistacs
     },
+    {
+      path:'*',
+      component:NotFound
+    }
 
 
   // {
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+      <h4>你访问的页面不存在</h4>
+      <div>
+          <router-link to="/">返回首页</router-link>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
