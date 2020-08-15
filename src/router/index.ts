dae366662013d825e29ae2b6f57265aad9f10d1a
龚箭 <15031617367@163.com>
2020-08-15 13:53:33 +0800

    创建了三个页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,43 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Money from "@/views/Money.vue";
+import Statistacs from "@/views/Statistacs.vue";
+import Labels from "@/views/Labels.vue";
 
 Vue.use(VueRouter)
 
   const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    {
+      path:'/',
+      redirect:'/money'
+    },
+    {
+      path: '/money',
+      name: 'money',
+      component: Money
+    },
+    {
+      path: '/',
+      name: 'labels',
+      component:Labels
+    },
+    {
+      path: '/',
+      name: 'statistacs',
+      component: Statistacs
+    },
+
+
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+  ]
 
 const router = new VueRouter({
   routes
---
+++ src/views/Money.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        hello Money
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "Money.vue"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
