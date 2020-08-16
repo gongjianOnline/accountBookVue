0a400afa6d778808ea6d1a022fba885c89060384
龚箭 <15031617367@163.com>
2020-08-16 11:45:31 +0800

    创建了<Icon/>

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+// const path = require('path')
+import path from "path"
+import svgSpriteLoader from "svg-sprite-loader/plugin"
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+    chainWebpack:config=>{
+      const dir = path.resolve(__dirname,'src/assets/icons')
+
+      config.module
+        .rule("svg-sprite")
+        .test(/\.svg$/)
+        .include.add(dir).end()
+        .use("svg-sprite-loader").loader("svg-sprite-loader").options({extract:false}).end()
+      config.plugin("svg-sprite").use(svgSpriteLoader,[{plainSprite:true}])
+      config.module.rule('svg').exclude.add(dir)
+
+
+
+    }
 }
