c43c5d5f91921bab550d531cf472b9d678581790
龚箭 <15031617367@163.com>
2020-08-17 21:15:14 +0800

    解决了 SVG 自带 fill 属性的问题

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
-// const path = require('path')
-import path from "path"
-import svgSpriteLoader from "svg-sprite-loader/plugin"
+const path = require('path')
+// import path from "path"
+// import svgSpriteLoader from "svg-sprite-loader/plugin"
 module.exports = {
   lintOnSave: false,
     chainWebpack:config=>{
@@ -11,7 +11,10 @@ module.exports = {
         .test(/\.svg$/)
         .include.add(dir).end()
         .use("svg-sprite-loader").loader("svg-sprite-loader").options({extract:false}).end()
-      config.plugin("svg-sprite").use(svgSpriteLoader,[{plainSprite:true}])
+        .use("svgo-loader").loader('svgo-loader')
+        .tap(options => ({...options,plugins:[{removeAttrs:{attrs:"fill"}}]}))
+        .end()
+      config.plugin("svg-sprite").use("svg-sprite-loader/plugin",[{plainSprite:true}])
       config.module.rule('svg').exclude.add(dir)
 
 
