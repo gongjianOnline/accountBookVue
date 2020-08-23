ed6184ca5f3df3b7d5e74594df3a518c80db3923
龚箭 <15031617367@163.com>
2020-08-23 20:31:18 +0800

    添加了全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div>
+  <div id="app">
     <router-view></router-view>
   </div>
 </template>
 
 <style lang="scss">
+@import "./assets/style/helper.scss";
+@import "./assets/style/reset.scss";
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;;
 }
 body{
-  line-height: 1.5;
+
 }
 a{
   text-decoration: none;
   color: inherit;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  line-height: 1.5;
+  font-family: $font-hei;
 }
 #nav {
   padding: 30px;
