e9dbaba2b1e21fc0b85a778bf8079eb00b98ddaf
李承斌 <15031617367@163.com>
2020-09-01 16:39:27 +0800

    记账页模块化完成

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,28 +1,30 @@
 <template>
-    <div class="nav-wrapper">
-        <div class="content">
-           <slot></slot>
-        </div>
-        <Nav></Nav>
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
+      <slot></slot>
     </div>
+    <Nav></Nav>
+  </div>
 </template>
 
 <script lang="ts">
   export default {
-    name: "layout"
+    name: "layout",
+    props: ['classPrefix']
   }
 </script>
 
 <style lang="scss" scoped>
-    .nav-wrapper{
-        border: 1px solid green;
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-    }
-    .content{
-        border: 1px solid blue;
-        overflow: auto;
-        flex-grow: 1;
-    }
+  .layout-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  .content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
