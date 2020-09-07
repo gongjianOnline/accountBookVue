72aa85ea5319d151dc9b16fdfbc462c94a661bd0
李承斌 <15031617367@163.com>
2020-09-07 17:31:38 +0800

    update

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
       <Layout>
-          hello label
+        <ol>
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ol>
       </Layout>
   </div>
 </template>
