e44e1fa91558b92195d7d355bac2e7443afb37ac
李承斌 <15031617367@163.com>
2020-09-18 14:16:29 +0800

    封装record的增操作

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ Vue.config.productionTip = false
 Vue.component("Nav",Nav)
 Vue.component("Layout",Layout)
 Vue.component("Icon",Icon)
-
+/**全局变量太多,过度依赖window***/
 // tag store
 window.tagList = tagListModel.fetch();
 window.createTag = (name: string)=>{
